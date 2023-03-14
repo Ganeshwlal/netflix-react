71d2b8c2257552bc6cbad9eca3eea7df9eb4ecce
ganeshwlal <ganeshwlal874@gmail.com>
2023-03-14 10:49:54 +0530

    netflix

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+
+
+
+  return (
+    
+    <div className='nav'>
+       <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="" />
+    </div>
+  )
+}
+
+export default Nav
